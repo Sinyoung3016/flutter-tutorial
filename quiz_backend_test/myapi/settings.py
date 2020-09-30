8f36354c92fa3dc50e866c3525b5773506c89a87
Sinyoung3016 <tasha0415@naver.com>
2020-09-30 18:13:09 +0900

    Update quiz_backend_test : Depoly

--- quiz_backend_test/myapi/settings.py
+++ quiz_backend_test/myapi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1i+0&g(plq&!kqydcgl2@hpws)1@1bz$%*3(9-=qa#*b(_u126'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '1i+0&g(plq&!kqydcgl2@hpws)1@1bz$%*3(9-=qa#*b(_u126')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'myapi.urls'
